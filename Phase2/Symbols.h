235125468f425c8792d133baed7a4d6d3c1e7194
OmarMahmoud11 <akatskiamv65@gmail.com>
2024-12-22 16:01:00 +0200

    create symbols file for constants

---
+++ Phase2/Symbols.h
@@ -0,0 +1,8 @@
+#ifndef COMPILER_PROJECT_SYMBOLS_H
+#define COMPILER_PROJECT_SYMBOLS_H
+#include <iostream>
+#include <bits/stdc++.h>
+
+constexpr const char* EPSILON = "epsilon"; // String literal for epsilon
+
+#endif //COMPILER_PROJECT_SYMBOLS_H
