fc92cca70cfdc1856672d200f4d79bf93ab7847b
OmarMahmoud11 <akatskiamv65@gmail.com>
2024-12-23 17:33:06 +0200

    Add new test to check first and follow and parsing table

--- Phase2/ParsingTable/main.cpp
+++ Phase2/ParsingTable/main.cpp
@@ -6,10 +6,33 @@
 #include <map>
 #include <vector>
 #include "ParsingTable.h"
+#include "../FirstAndFollow/FirstAndFollow.h"
+#include "../LeftRecursionAndLeftFactoring/HandleLeftFactoring.h"
+#include "../LeftRecursionAndLeftFactoring/HandleLeftRecursion.h"
 
+void print_grammar(std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar){
+    for (const auto& [non_terminal, productions] : grammar) {
+        std::cout << non_terminal << " -> ";
+
+        for (size_t i = 0; i < productions.size(); ++i) {
+            // Print each production
+            for (size_t j = 0; j < productions[i].size(); ++j) {
+                std::cout << productions[i][j];
+                if (j < productions[i].size() - 1) {
+                    std::cout << " ";  // Separate symbols in the production
+                }
+            }
+            if (i < productions.size() - 1) {
+                std::cout << " | ";  // Separate productions
+            }
+        }
+
+        std::cout << std::endl;
+    }
+}
 void test1() {
     // Define grammar
-    std::map<std::string, std::vector<std::vector<std::string>>> grammar = {
+    std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar = {
             {"S", {{"R", "T"}}},
             {"R", {{"s", "U", "R", "b"}, {EPSILON}}},
             {"U", {{"u", "U"},           {EPSILON}}},
@@ -57,35 +80,78 @@ void test1() {
 
 void test2() {
     // Define grammar
-    std::map<std::string, std::vector<std::vector<std::string>>> grammar = {
-            {"E",     {{"T", "Edash"}}},
-            {"Edash", {{"+", "T", "Edash"}, {EPSILON}}},
-            {"T",     {{"F", "Tdash"}}},
-            {"Tdash", {{"*", "F", "Tdash"}, {EPSILON}}},
+    std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar = {
+            {"E",     {{"T", "E`"}}},
+            {"E`", {{"+", "T", "E`"}, {EPSILON}}},
+            {"T",     {{"F", "T`"}}},
+            {"T`", {{"*", "F", "T`"}, {EPSILON}}},
             {"F",     {{"(", "E", ")"},     {"id"}}}
     };
 
     // Define terminals and non-terminals
     std::set<std::string> terminals = {"+", "*", "(", ")", "id", EPSILON};
-    std::set<std::string> non_terminals = {"E", "Edash", "T", "Tdash", "F"};
+    std::set<std::string> non_terminals = {"E", "E`", "T", "T`", "F"};
+    std::vector<std::string> order_non_terminals = {"E", "E`", "T", "T`", "F"};
 
-    // Define first sets
-    std::map<std::string, std::set<std::string>> first = {
-            {"E",     {"(", "id"}},
-            {"Edash", {"+", EPSILON}},
-            {"T",     {"(", "id"}},
-            {"Tdash", {"*", EPSILON}},
-            {"F",     {"(", "id"}}
+//    Apply Left Recursion and Left Factoring
+    HandleLeftRecursion left_recursion(grammar,non_terminals,order_non_terminals);
+    left_recursion.eliminate_left_recursion();
+    grammar = left_recursion.getGrammar();
+
+    HandleLeftFactoring left_factoring(grammar);
+    left_factoring.eliminate_left_factoring();
+    grammar = left_factoring.getGrammar();
+
+//    Create first and follow
+    FirstAndFollow first_and_follow(grammar,terminals,non_terminals,"E");
+    first_and_follow.create_first_and_follow();
+    std::map<std::string, std::set<std::string>> first = first_and_follow.get_first();
+    std::map<std::string, std::set<std::string>> follow = first_and_follow.get_follow();
+
+    // Generate parsing table
+    auto parsingTable = ParsingTable::parsing_table_derivation(grammar, terminals, non_terminals, first, follow);
+
+    // Print parsing table for verification
+    for (const auto &non_terminal_entry: parsingTable) {
+        std::cout << "Non-terminal: " << non_terminal_entry.first << "\n";
+        for (const auto &terminal_entry: non_terminal_entry.second) {
+            std::cout << "  Terminal: " << terminal_entry.first << " -> [ ";
+            for (const auto &symbol: terminal_entry.second) {
+                std::cout << symbol << " ";
+            }
+            std::cout << "]\n";
+        }
+    }
+}
+void test3() {
+    // Define grammar
+    std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar = {
+            {"bexpr", {{"bexpr", "or", "bterm"},{"bterm"}}},
+            {"bterm", {{"bterm", "and", "bfactor"},{"bfactor"}}},
+            {"bfactor",{{"not","bfactor"},{"(","bexpr",")"},{"true"},{"false"}}}
     };
 
-    // Define follow sets
-    std::map<std::string, std::set<std::string>> follow = {
-            {"E",     {"$", ")"}},
-            {"Edash", {"$", ")"}},
-            {"T",     {"+", "$", ")"}},
-            {"Tdash", {"+", "$", ")"}},
-            {"F",     {"+", "*", "$", ")"}}
-    };
+    // Define terminals and non-terminals
+    std::set<std::string> terminals = {"or", "and","not" ,"(", ")", "true","false", EPSILON};
+    std::set<std::string> non_terminals = {"bexpr", "bterm","bfactor"};
+    std::vector<std::string> order_non_terminals = {"bexpr", "bterm","bfactor"};
+
+//    Apply Left Recursion and Left Factoring
+    HandleLeftRecursion left_recursion(grammar,non_terminals,order_non_terminals);
+    left_recursion.eliminate_left_recursion();
+    grammar = left_recursion.getGrammar();
+    non_terminals = left_recursion.getNonTerminals();
+
+    HandleLeftFactoring left_factoring(grammar);
+    left_factoring.eliminate_left_factoring();
+    grammar = left_factoring.getGrammar();
+
+
+//    Create first and follow
+    FirstAndFollow first_and_follow(grammar,terminals,non_terminals,"bexpr");
+    first_and_follow.create_first_and_follow();
+    std::map<std::string, std::set<std::string>> first = first_and_follow.get_first();
+    std::map<std::string, std::set<std::string>> follow = first_and_follow.get_follow();
 
     // Generate parsing table
     auto parsingTable = ParsingTable::parsing_table_derivation(grammar, terminals, non_terminals, first, follow);
@@ -105,6 +171,7 @@ void test2() {
 
 int main() {
 //    test1();
-    test2();
+//    test2();
+    test3();
     return 0;
 }
