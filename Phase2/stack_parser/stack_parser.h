b2b8ff39eb10148faca2bfdf1684e70bb1732f05
AbdelRahman-Elsayed2 <abdelrahmanelsayd223@gmail.com>
2024-12-23 06:58:53 +0200

    stack of parser is done

---
+++ Phase2/stack_parser/stack_parser.h
@@ -0,0 +1,36 @@
+#include <map>
+#include <string>
+#include <vector>
+#include<stack>
+#include <queue>
+#include <set>
+#ifndef STACK_PARSER_H
+#define STACK_PARSER_H
+using namespace std;
+class stack_parser{
+    private:
+       map<string,map<string, vector<string>>> parserTable;
+       stack<string> parserStack;
+       queue<string> actions;
+       string startSymbol;
+       int index ;
+       vector<string>tokens;
+       vector<string>prodaction;
+    public:
+       stack_parser(map<string,map<string, vector<string>>> parserTable, string startSymbol, vector<string>tokens);
+       queue<string> get_actions();
+       void parse(
+          set<std::string> terminals
+       );
+       string get_next_token();
+       void display_action();
+       bool is_terminal(
+         std::set<std::string> terminals,
+         string in
+       );
+       string string_stack(stack<string> stk);
+       vector<string> get_prodaction();
+
+};
+
+#endif
