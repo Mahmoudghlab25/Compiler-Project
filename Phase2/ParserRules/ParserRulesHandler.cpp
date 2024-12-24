289029c9dc4677b11a41cee6cea6c680850cb014
OmarMahmoud11 <akatskiamv65@gmail.com>
2024-12-24 16:30:41 +0200

    commit

--- Phase2/ParserRules/ParserRulesHandler.cpp
+++ Phase2/ParserRules/ParserRulesHandler.cpp
@@ -4,18 +4,33 @@ using namespace std;
 
 Grammar ParserRulesHandler::parseRules(const vector<string>& parserRules) {
     LexicalRulesHandler lexRulesHandler;
+    auto rules = lexRulesHandler.readRules(
+            R"(D:\Level_4_Semester_1\Compilers\Project\Compiler-Project\Phase2\rules.txt)");
+    rules.pop_back();
     // rules for extracting tokens from defined parser rules
-    vector<string> rules = {
-        "letter = a-z|A-Z",
-        "digit = 0-9",
-        "symbol = !|\"|#|$|%|&|'|\\(|\\)|\\*|\\+|,|\\-|.|/|\\:|;|<|\\=|>|?|@|\\[|\\\\|\\]|^|_|`|\\{|\\||\\}|~",
-        "start: #",
-        "nonterminal: letter(letter|digit|_)*",
-        "terminal: '(symbol|letter|digit)+'",
-        "assign: \\:\\:\\=",
-        "or: \\|",
-        "lambda: \\L"
-    };
+//    vector<string> rules = {
+//        "letter = a-z|A-Z",
+//        "digit = 0-9",
+//        "symbol = !|\"|#|$|%|&|'|\\(|\\)|\\*|\\+|,|\\-|.|/|\\:|;|<|\\=|>|?|@|\\[|\\\\|\\]|^|_|`|\\{|\\||\\}|~",
+//        "start: #",
+//        "nonterminal: letter(letter|digit|_)*",
+//        "terminal: '(symbol|letter|digit)+'",
+//        "assign: \\:\\:\\=",
+//        "or: \\|",
+//        "lambda: \\L"
+//    };
+//    letter = a-z|A-Z
+//    digit = 0-9
+//    id: letter(letter|digit)*
+//    digits = digit+
+//    {boolean int float}
+//    num: digit+|digit+.digits(\L|Edigits)
+//    relop: \=\=|!\=|>|>\=|<|<\=
+//    assign: \=
+//    { if else while }
+//    [; , \( \) { }]
+//    addop: \+|\-
+//    mulop: \*|/
     lexRulesHandler.extractKeywords(rules);
     lexRulesHandler.extractPunctuation(rules);
     lexRulesHandler.extractStatements(rules);
@@ -32,6 +47,10 @@ Grammar ParserRulesHandler::parseRules(const vector<string>& parserRules) {
     );
     auto minTransitionTable = MinimizeDFA
         ::getReducedTransitionTable(minStates);
+    cout<<"Finish Min DFA\n";
+    if (minStates.empty()) {
+        throw std::runtime_error("minDFA generated an empty states set.");
+    }
     LexicalAnalyzer lexer(parserRules, minStates);
     vector<string> tokens = lexer.analyze();
 
