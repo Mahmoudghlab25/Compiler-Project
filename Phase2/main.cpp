3e270fd81fec50e4642f0bf95afc9970ff3a97f1
MohamedAmr982 <mohamedmohamedamr982@hotmail.com>
2024-12-24 02:02:43 +0200

    main file for phase 2

--- Phase2/main.cpp
+++ Phase2/main.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+#include "ParserRules/ParserRulesHandler.h"
+#include "../Phase1/FileReader.cpp"
+
+using namespace std;
+
+int main() {
+    ParserRulesHandler p;
+    FileReader reader;
+    string path = R"(D:\compiler_phase2_clion\Phase2\parser_input.txt)";
+    auto lines = reader.readLines(path);
+    p.parseRules(lines);
+}
+
